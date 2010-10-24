4ac31cbadef969756fdf1287fce621e18206de92
Alexander Lourier <aml@rulezz.ru>
2010-10-24 13:02:27 +0400

    Attacks: posting comments, fetching posts

--- jb/attack.py
+++ jb/attack.py
@@ -39,5 +39,43 @@ def create_post(target, title, body):
     except IOError as e:
         store_error(target, e)
 
+def fetch_post(target, id):
+    cnn = HTTPConnection()
+    try:
+        cnn.connect((target["host"], 80))
+        try:
+            start = time.time()
+            req = cnn.get("/posts/%s" % id)
+            res = cnn.perform(req)
+            if res.status_code != 200:
+                store_error(target, "fetch_post: %d" % res.status_code)
+            print "get time=%f" % (time.time() - start)
+        finally:
+            cnn.close()
+    except IOError as e:
+        store_error(target, e)
+
+def comment_post(target, id, body):
+    content = "body=%s" % (urlencode(body))
+    if len(content) > 1024 * 1024:
+        store_error(target, "request_too_large")
+        return
+    cnn = HTTPConnection()
+    try:
+        cnn.connect((target["host"], 80))
+        try:
+            start = time.time()
+            req = cnn.post("/posts/%s/comments" % id, content)
+            req.add_header("Content-type", "application/x-www-form-urlencoded")
+            req.add_header("Content-length", len(content))
+            res = cnn.perform(req)
+            if res.status_code != 302:
+                store_error(target, "create_comment: %d" % res.status_code)
+            print "comment len=%d, time=%f" % (len(content), time.time() - start)
+        finally:
+            cnn.close()
+    except IOError as e:
+        store_error(target, e)
+
 def create_post_with_large_title(target):
     create_post(target, "Title", random_string(950000))
